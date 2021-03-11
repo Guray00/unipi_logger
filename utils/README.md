b698c77472c4417d2b0e3650aa4e20378293fcc2
Marco <30523962+Guray00@users.noreply.github.com>
2021-03-11 12:52:45 +0100

    Aggiunto python finder

---
+++ utils/README.md
@@ -0,0 +1,2 @@
+# Find
+Utility che permette di individuare la cartella di installazione attuale di python. Utile per le installazioni Windows.
---
+++ utils/find.py
@@ -0,0 +1,9 @@
+import sys
+import platform
+import imp
+
+print("Python EXE     : " + sys.executable)
+print("Architecture   : " + platform.architecture()[0])
+
+
+input("\n\nPress ENTER to quit")
